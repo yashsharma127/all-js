9d325da2b80867391344eb16ac1cb12daa79a5fe
yashsharma127 <yashshar.2323@gmail.com>
2023-07-12 21:22:09 +0530

    a

--- n/index.js
+++ n/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-app.use(express.json())
-
+app.use(cors())
+app.use(cors())
 let notes = [
   {
     id: 1,
