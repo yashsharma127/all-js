327010c507a85fb56d4ecadce1f80e38832566bb
yashsharma127 <yashshar.2323@gmail.com>
2023-07-15 23:50:21 +0530

    removing comments

--- n/index.js
+++ n/index.js
@@ -10,37 +10,6 @@ app.use(express.json())
 
 let notes = []
 
-// app.get('/api/notes', (req, res) => {
-//   res.json(notes)
-// })
-
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
-
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
-
-//   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
-//     })
-//   }
-
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     date: new Date(),
-//     id: generateId(),
-//   }
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
